ac66a9f47638059e7cd724d9b2722a62acf21fa8
Petrik Diak <diak@petrik.hu>
2026-01-30 08:46:27 +0100

    Add project files.

---
+++ Digitalis_NyomozoIroda/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Digitalis_NyomozoIroda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
